73b8dbd57af3f2e2b690de6790d4a87e42106c77
Lucas Gonzalez Pearson <lucasgp+github@gmail.com>
2014-02-13 03:39:09 -0300

    fetch events async

--- src/com/srlucas/github/eventviewer/MainActivity.java
+++ src/com/srlucas/github/eventviewer/MainActivity.java
@@ -1,20 +1,42 @@
 package com.srlucas.github.eventviewer;
 
+import org.json.JSONArray;
+import org.json.JSONObject;
+
 import android.app.Activity;
 import android.os.Bundle;
+import android.widget.ListView;
 
+import com.loopj.android.http.JsonHttpResponseHandler;
 import com.srlucas.github.R;
+import com.srlucas.github.eventviewer.network.github.GithubRestClient;
 
 /** Main Activity for our event viewer
  * 
  * @author srlucas
  */
 public class MainActivity extends Activity {
+	
+	ListView events;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
+		
+		fetchEventsAsync();
+	}
+	
+	/**
+	 * Fetch events to show and populate ListView
+	 */
+	private void fetchEventsAsync() {
+		GithubRestClient.get("events", null, new JsonHttpResponseHandler() {
+            @Override
+            public void onSuccess(JSONArray events) {
+            	// update listview
+            }
+        });
 	}
 
 }
