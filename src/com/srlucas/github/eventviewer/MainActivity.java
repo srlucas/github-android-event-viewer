64e3273aabff087036d6ae17bfac69f015632fb0
Lucas Gonzalez Pearson <lucasgp+github@gmail.com>
2014-02-13 05:02:28 -0300

    Added Adapter and all required code to make it populate the listview

--- src/com/srlucas/github/eventviewer/MainActivity.java
+++ src/com/srlucas/github/eventviewer/MainActivity.java
@@ -1,5 +1,7 @@
 package com.srlucas.github.eventviewer;
 
+import java.util.ArrayList;
+
 import org.json.JSONArray;
 
 import android.app.Activity;
@@ -9,6 +11,7 @@ import android.widget.ListView;
 import com.loopj.android.http.JsonHttpResponseHandler;
 import com.loopj.android.http.RequestParams;
 import com.srlucas.github.R;
+import com.srlucas.github.eventviewer.model.Event;
 import com.srlucas.github.eventviewer.network.github.GithubRestClient;
 
 /** Main Activity for our event viewer
@@ -17,14 +20,31 @@ import com.srlucas.github.eventviewer.network.github.GithubRestClient;
  */
 public class MainActivity extends Activity {
 	
-	ListView events;
+	/** Will convert Event model to listview. */
+	public EventsAdapter adapter;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
 		
+		prepareListView();
+		
 		fetchEventsAsync(0);
+	}
+	
+	/** Sets everything to that the EventsAdapter is properly attached to the
+	 * ListView.
+	 */
+	private void prepareListView() {
+		// Construct the data source
+		ArrayList<Event> ghEvents = new ArrayList<Event>();
+		// Create the adapter to convert the array to views
+		this.adapter = new EventsAdapter(this, ghEvents);
+		
+		// Attach the adapter to a ListView
+		ListView listView = (ListView) findViewById(R.id.listview);
+		listView.setAdapter(adapter);
 	}
 	
 	/**
@@ -39,11 +59,11 @@ public class MainActivity extends Activity {
 				new JsonHttpResponseHandler() {
 		            @Override
 		            public void onSuccess(JSONArray events) {
+		            	
 		            	// update listview
+		            	adapter.addAll(Event.fromJson(events));
 		            }
 				}
 		); // ascii art
-		
 	}
-
 }
